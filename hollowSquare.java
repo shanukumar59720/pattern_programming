5768f9b3d2e45b2d506dad2783fd8b68191076c2
shanukumar59720 <shanukumar59720>
2024-03-28 08:56:25 +0530

    pattern programming

--- hollowSquare.java
+++ hollowSquare.java
@@ -7,12 +7,20 @@
  *   
  */
 
+import java.util.Scanner;
+
 public class hollowSquare {
     public static void main(String[] args) {
-        for(int i=0; i<5; i++){
-            for (int j = 0; j <5; j++) {
-                if(i==0 || i == 4 || j==0 || j==4){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size: ");
+        int size = sc.nextInt();
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (i == 0 || j == 0 || i == size-1 || j == size-1) {
                     System.out.print("* ");
+                } else {
+                    System.out.print("  ");
                 }
             }
             System.out.println();
