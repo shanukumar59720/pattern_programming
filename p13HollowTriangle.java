a2eb3913281b5790b31dd8b5f09c785602f84465
shanukumar59720 <shanukumar59720>
2024-05-08 23:45:44 +0530

    update pattern programming

--- p13HollowTriangle.java
+++ p13HollowTriangle.java
@@ -14,13 +14,10 @@ public class p13HollowTriangle {
     public static void main(String[] args) {
         int num = 5;
 
-        for (int i = 1; i <=5; i++) {
-            System.out.print("* ");            
-            for (int j = 1; j < args.length; j++) {
-                
+        for (int i = 5; i >=1; i--) {                       
+            for (int j = 5; j>=i; j--) {
+                if()
             }
-
-
             System.out.println();
         }
     }
